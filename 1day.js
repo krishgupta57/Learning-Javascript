f961707ca5de62d2918380e29f8fe2c757f901a3
krishgupta57 <krishgupta7898@gmail.com>
2025-08-05 18:22:23 +0530

    Basic

---
+++ 1day.js
@@ -0,0 +1,11 @@
+console.log("external file")
+console.log(123456789);
+console.log(true);
+console.log(23.2558);
+console.log('a');
+
+alert("wrong input")
+
+document.write("from JS file")
+
+document.write("<h1> from JS file </h1>")
