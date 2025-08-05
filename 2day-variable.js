bc5f90f97a13f64a4161b6581c3a231dec3204a7
krishgupta57 <krishgupta7898@gmail.com>
2025-08-05 19:53:08 +0530

    Variables

---
+++ 2day-variable.js
@@ -0,0 +1,18 @@
+let num1 = 10
+let num2 = 20
+// let num3 = num1+num2;
+
+alert(num1+num2)
+
+// WAP tp find area of circle
+
+let r = 3
+
+console.log("area of circle " +3.14*r*r)
+
+//WAP to find area of rectangle
+
+let l = 10
+let b = 20
+
+document.write(l*b)
