f43a1b0e907c92bbf0a784669ef1ff270d06c922
krishgupta57 <krishgupta7898@gmail.com>
2025-08-07 20:52:29 +0530

    WPA

---
+++ 3day-userinput.js
@@ -0,0 +1,37 @@
+//WPA to take 2 user input and add them?
+/*
+let num1 = parseInt(prompt("Enter the first number"))
+let num2 = parseInt(prompt("Enter the second number"))
+
+console.log(num1+num2)
+*/
+
+//WAP to find the area of triangle from user input?
+/*
+let b = parseInt(prompt("Enter the base"))
+let h = parseInt(prompt("Enter the height"))
+
+console.log(0.5*b*h)
+*/
+
+//WAP to take 2 user input and find square of both the add both the square
+/*
+let a = parseInt(prompt("enter the first no."))
+let b = parseInt(prompt("enter the second no."))
+
+console.log(a*a)
+console.log(b*b)
+
+console.log((a*a)+(b*b))
+*/
+
+//WAP to swap 2 value?
+/*
+let a = parseInt(prompt("Enter the first no."))
+let b = parseInt(prompt("Enter the second no."))
+
+let c = a
+a=b
+b=c
+console.log(a+" "+b)
+*/
