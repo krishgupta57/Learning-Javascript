4ffc46b1178b038a28b8161eed25e5c8581f8c00
krishgupta57 <krishgupta7898@gmail.com>
2025-08-14 19:29:16 +0530

    Conditional

---
+++ 6day-nested_if_else.js
@@ -0,0 +1,17 @@
+/*
+let age = parseInt(prompt("Enter your age"))
+
+if(age>=18){
+    let n = parseInt(prompt("Press 1 for Indian"))
+    if(n==1){
+        console.log("Eligible for Voting");
+    }
+    else{
+        console.log("Not Eligible");
+    }
+}
+else{
+    console.log("You are Underage!...");
+}
+*/
+
