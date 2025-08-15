2328584b432f84df863685f85776e92f4339a875
krishgupta57 <krishgupta7898@gmail.com>
2025-08-16 01:04:53 +0530

    Loops

---
+++ 6day-do_while.js
@@ -0,0 +1,31 @@
+/*
+let start = 1
+
+do{
+    console.log("Krish");
+    start++
+}
+while(start<11)
+*/
+
+
+//WAP to print a table of any number
+/*
+let x = parseInt(prompt("Enter a number"))
+let y = 1
+
+do{
+    console.log(x*y);
+    y++;
+}
+while(y<11)
+*/
+
+
+//WAP to take input and if user put 0 then terminate
+
+do{
+    let num = parseInt(prompt("Enter a number"))
+}
+while(num != 0)
+
