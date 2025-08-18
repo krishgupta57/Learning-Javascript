2db52db64d07117d662277af4bba1f17b64c5612
krishgupta57 <krishgupta7898@gmail.com>
2025-08-18 19:56:23 +0530

    Loop

---
+++ 7day-while.js
@@ -0,0 +1,29 @@
+
+/*
+let st = 1
+
+while(st<=10){
+    console.log(st);
+    st++;
+}
+*/
+
+//WAP to sum the number from 1 to 10 using while loop?
+/*
+let x = 1
+let y = 0
+while(x<=10){
+    y = x+y
+    x++
+}
+console.log(y);
+*/
+
+//WAP to print even numner from 1 to 15 using while loop?
+let x = 1
+while(x<16){
+    if(x%2==0){
+        console.log(x);
+    }
+    x++
+} 
