e2eb7c73cb90d43e24e3044d6a7ff61f2695a8b2
krishgupta57 <krishgupta7898@gmail.com>
2025-09-01 19:50:21 +0530

    Document Objet Model

---
+++ 16day-Dom3.js
@@ -0,0 +1,26 @@
+// document.getElementById('head').textContent = "Krish"
+
+let head1 = document.getElementById('head')
+
+head1.innerHTML = "<u> Gupta </u>"
+
+head1.style.cssText = "color:red; background-color:yellow; padding:20px"
+
+
+//new tag created from js
+let head2 = document.createElement('h2')
+console.log(head2);    //to check that tag is created or not
+
+head2.textContent = "this is new tag created from js"
+
+document.body.append(head2)
+
+//unorderd list created from js
+
+let ulist = document.createElement('ul')
+let list = document.createElement('li')
+
+list.textContent = "Home"
+ulist.append(list)
+
+document.body.append(ulist)
