f0b97ff97141a1db6557a27abe3c8f2dda1e6f26
krishgupta57 <krishgupta7898@gmail.com>
2025-09-02 20:00:17 +0530

    Document Object Model

---
+++ 17day-Dom4_Event.js
@@ -0,0 +1,26 @@
+
+
+function fun(){
+    alert("Onclick working....")
+}
+
+function fun1(){
+    let head = document.getElementById('head')
+    head.innerHTML = 'Welcome to JS'
+}
+
+
+function Red(){
+    document.getElementById('cname').textContent="Red"
+    document.body.style.backgroundColor = "red"
+}
+
+function Yellow(){
+    document.getElementById('cname').textContent="Yellow"
+    document.body.style.backgroundColor = "yellow"
+}
+
+function Blue(){
+    document.getElementById('cname').textContent="Blue"
+    document.body.style.backgroundColor = "blue"
+}
