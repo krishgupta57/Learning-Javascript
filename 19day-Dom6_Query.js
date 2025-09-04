f4b53a90fd04928eb6ffe91ff9196de61754dfb0
krishgupta57 <krishgupta7898@gmail.com>
2025-09-04 19:57:35 +0530

    Document Object Model

---
+++ 19day-Dom6_Query.js
@@ -0,0 +1,21 @@
+let head = document.getElementsByTagName('h1')
+//console.log(head);
+
+head[0].innerHTML = "ecjmosjc"
+
+//Query Selector
+
+let h = document.querySelector('.head2')
+h.innerHTML = "fine"
+
+//to target tag
+let x = document.querySelector('h3')
+console.log(x);
+
+//to target ID
+let y = document.querySelector('#fun')
+console.log(y);
+
+//to target Class
+let z = document.querySelector('.fun1')
+console.log(z);
