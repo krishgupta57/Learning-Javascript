221fcc12da38bb705d16e7ca35a0af43c6ad61f9
krishgupta57 <krishgupta7898@gmail.com>
2025-09-08 19:52:18 +0530

    Document Object Model

---
+++ 20day-Dom7_Event3.js
@@ -0,0 +1,30 @@
+function mob(){
+    let image = document.querySelector('#image')
+    image.src = "img/phone.webp"
+}
+
+function laptop(){
+    let image = document.querySelector('#image')
+    image.src = "img/laptop.webp"
+}
+
+function tablet(){
+    let image = document.querySelector('#image')
+    image.src = "img/tablet.webp"
+}
+function watch(){
+    let image = document.querySelector('#image')
+    image.src = "img/watch.webp"
+}
+
+
+//text change
+function welcome(){
+    let wel = document.getElementById('welcome')
+    wel.textContent = "Good Bye"
+}
+
+function goodbye(){
+    let wel = document.getElementById('welcome')
+    wel.textContent = "Welcome"
+}
