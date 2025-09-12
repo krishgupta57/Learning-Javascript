600325d0e5336f902b6102022b4c6281a6106770
krishgupta57 <krishgupta7898@gmail.com>
2025-09-12 19:47:02 +0530

    Form Handling

--- 22day-Form3.js
+++ 22day-Form3.js
@@ -2,6 +2,10 @@ function frmdata(){
     let name = document.querySelector('#name').value
     let age = document.querySelector('#age').value
     let contact = document.querySelector('#contact').value
+    let email = document.querySelector('#email').value
+    let pass = document.querySelector('#pass').value
+    let cpass = document.querySelector('#cpass').value
+
 
     if(name == ""){
         alert("Please enter your name")
@@ -35,6 +39,39 @@ function frmdata(){
     else if(contact.length != 10){
         alert("Please enter your 10 digit mobile number")
         document.querySelector('#contact').focus()
+        return false
+    }
+
+    else if(email == ""){
+        alert("Please enter your email")
+        document.querySelector('#email').focus()
+        return false
+    }
+    else if(!(email.includes('@gmail.com'))){
+        alert('Enter the correct email')
+        document.querySelector('#email').focus()
+        return false
+    }
+
+    else if(pass == ""){
+        alert("Please enter the password")
+        document.querySelector('#pass').focus()
+        return false
+    }
+    else if(!(pass.match(/[!@#$%^&*]/))){
+        alert("Please enter any special Charcter")
+        document.querySelector('#pass').focus()
+        return false
+    }
+
+    else if(cpass == ""){
+        alert("Please enter the confirm password")
+        document.querySelector('#cpass').focus()
+        return false
+    }
+    else if(pass != cpass){
+        alert("Password didn't match")
+        document.querySelector('#cpass').focus()
         return false
     }
 }
