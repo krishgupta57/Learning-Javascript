15926d71b6a14a5107c961330e2185910efd550f
krishgupta57 <krishgupta7898@gmail.com>
2025-10-09 19:18:32 +0530

    Math Function

---
+++ 40day-Math.js
@@ -0,0 +1,24 @@
+// use to perform mathematical operation and calculation
+// .trunk() is use to remove the decimal present in the value
+console.log(Math.trunc(17.55))
+
+// .random is use to generates a pseudorandom floating-point number
+console.log(Math.random())
+
+// .min() is ised to find the smallest no. from the arguments
+console.log(Math.min(10,20,30))
+
+// .min() is used to find the smallest no. from the arguments
+console.log(Math.max(10,20,30))
+
+// .floor is use to round down the no. to the negative side
+console.log(Math.floor(5.8))
+
+// .ceil is use to round up the no. to the positive side
+console.log(Math.ceil(5.8))
+
+// .pow is used to to make the square of the no. // we pass 2 argumant .pow(number, square)
+console.log(Math.pow(5,3))
+
+// .sqrt is use to fund the squareroot of the following no. 
+console.log(Math.sqrt(25))
